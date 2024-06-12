282a580c2c11a1a0d07a0ddc883d3fd9ebf97aea
Hector Ulises Vazquez Pardo <hectorvazquez@grupovidanta.com>
2024-06-12 17:55:53 -0600

    finalizando el diseño, empezando con JS

---
+++ javascript/js.js
@@ -0,0 +1,6 @@
+let btn = document.querySelector('input');
+let tabla = document.querySelector('tbody');
+btn.addEventListener('click',function(e){
+    e.preventDefault();
+    console.log("hola");
+})
