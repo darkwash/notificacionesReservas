7db4f59b1f6f67549cad4d6fb5d77c79ed68c154
Hector Vazquez <correo@correo.com>
2024-08-15 13:59:46 -0600

    Se optimiza imagenes y se agrega objeto de tabla filtrada

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,42 @@
 const btn = document.querySelector("input");
 const tabla = document.querySelector("tbody");
-tabla.innerHTML = "";
+
 let fila;
 document.addEventListener("DOMContentLoaded", showTable);
+
 function showTable() {
-  fetch("../src/assets/listado.json")
+  fetch("src/assets/listado.json")
     .then((response) => response.json())
     .then((plaza) => creatabla(plaza))
     .catch(() => ocultartable());
 }
+
+// si no hay datos  o el archivo no es leido, se ejecuta la funcion
 function ocultartable() {
   tabla.innerHTML =
     "<tr><td colspan=3 >No existe ningun elemento en la tabla, revisar el archivo JSON</td> </tr>";
   const boton = document.querySelector(".btn");
   boton.classList.add("btn-desactivado");
 }
+
+// crea la tabla una vez se leyo el archivo
 function creatabla(datos) {
-  // console.log(datos.filter((lugar) => lugar.plaza == 'mazatlan')); linea para filtrar por plazas, pendiente guardar en arreglos por plaza para manipular el envio de email
+  const plazas = {
+    mazatlan: [],
+    cabos: [],
+    "riviera maya": [],
+    acapulco: [],
+    peñasco: []
+  };
+
+  datos.forEach(lugar => {
+    if (plazas[lugar.plaza]) {
+      plazas[lugar.plaza].push(lugar.idReserva);
+    }
+  });
+
   let i = 0;
+  tabla.innerHTML = "";
   while (i < datos.length) {
     fila = document.createElement("tr");
     fila.innerHTML = `<td> ${datos[i].idReserva}</td>
@@ -26,7 +45,14 @@ function creatabla(datos) {
     tabla.appendChild(fila);
     i++;
   }
+  const plazasFiltradas = Object.fromEntries(
+    Object.entries(plazas).filter(([key, value]) => value.length > 0)
+  );
+
+  console.log(plazasFiltradas);
+  // console.log(plazas);
 }
+
 btn.addEventListener("click", function (e) {
   e.preventDefault();
   console.log("Enviando correo");
